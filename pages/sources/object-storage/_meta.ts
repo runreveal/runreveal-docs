8981a3a2125e82fcd2096aa6b83cefb8b5f4373d
Larry Archer <larry@runreveal.com>
2025-09-09 18:10:13 -0500

    Updates on Cloudflare sources
    - New page for Cloudflare Firewall source
    - Adding a note about 405 errors from Cloudflare when setting up an R2 source

--- pages/sources/object-storage/_meta.ts
+++ pages/sources/object-storage/_meta.ts
@@ -2,5 +2,6 @@ export default {
     "s3": "AWS S3 Bucket",
     "external-s3": "AWS S3 with custom SQS",
     "azure": "Azure Blob Storage",
-    "gcs": "Google Cloud Storage"
+    "gcs": "Google Cloud Storage",
+    "r2": "Cloudflare R2 Storage"
 }
