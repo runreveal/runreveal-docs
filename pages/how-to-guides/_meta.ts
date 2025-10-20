5b14c4aab9ebe1231a03c1af724fb125e404216a
Jonathan McIntyre <11054779+jonathanmcintyre1@users.noreply.github.com>
2025-10-20 16:13:57 -0600

    hide onboarding guid
    hide onboarding guide for now

--- pages/how-to-guides/_meta.ts
+++ pages/how-to-guides/_meta.ts
@@ -1,5 +1,5 @@
 export default {
-  "onboarding-guide": "Onboarding Guide",
+  // "onboarding-guide": "Onboarding Guide", // Hidden
   "collect-nginx-logs": "Collect Nginx Logs",
   "detections-signals-alerts-quick-start": "Quick Start: Detections, Signals, and Alerts",
   "kubernetes-logs": "Kubernetes Logs",
