9c883f46f2265901b35d243a2ab3d096f70e5f58
Jonathan McIntyre <11054779+jonathanmcintyre1@users.noreply.github.com>
2025-10-20 16:17:31 -0600

    remove onboarding guide
    remove onboarding guide entirely

--- pages/how-to-guides/_meta.ts
+++ pages/how-to-guides/_meta.ts
@@ -1,5 +1,4 @@
 export default {
-  // "onboarding-guide": "Onboarding Guide", // Hidden
   "collect-nginx-logs": "Collect Nginx Logs",
   "detections-signals-alerts-quick-start": "Quick Start: Detections, Signals, and Alerts",
   "kubernetes-logs": "Kubernetes Logs",
