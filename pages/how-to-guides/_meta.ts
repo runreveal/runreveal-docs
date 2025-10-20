0fb36f51c5ecdb8ba2a8e2c494c4a4338fdc4648
Jonathan McIntyre <11054779+jonathanmcintyre1@users.noreply.github.com>
2025-10-20 13:47:51 -0600

    Adding and updating custom views, onboarding guide, and updating scheduled prompts
    Adding docs for custom views, onboarding guide, and updating scheduled prompts docs.

--- pages/how-to-guides/_meta.ts
+++ pages/how-to-guides/_meta.ts
@@ -1,4 +1,5 @@
 export default {
+  "onboarding-guide": "Onboarding Guide",
   "collect-nginx-logs": "Collect Nginx Logs",
   "detections-signals-alerts-quick-start": "Quick Start: Detections, Signals, and Alerts",
   "kubernetes-logs": "Kubernetes Logs",
